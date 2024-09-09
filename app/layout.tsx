1dca02f3c253af06ba1403afd2cb4959328a01a7
Sweep <joshuapatrickchiu76@gmail.com>
2024-09-09 15:48:44 +0800

    added image package and minor bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 import { cn } from '@/lib/utils';
+
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -23,8 +25,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', 
-      fontSans.variable)}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable)}>
+        <ThemeProvider 
+          attribute="class" 
+          defaultTheme="dark"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
