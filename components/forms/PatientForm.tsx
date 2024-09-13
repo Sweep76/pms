1442aabc63ac6bce8bebe69699e120c40a1e2bea
Sweep <joshuapatrickchiu76@gmail.com>
2024-09-13 20:12:21 +0800

    feat: added async function

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -8,6 +8,7 @@ import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
 
 export enum FormFieldType {
   INPUT = 'input',
@@ -18,29 +19,31 @@ export enum FormFieldType {
   SELECT = 'select',
   SKELETON = 'skeleton',
 }
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
  
 const PatientForm = () => {
   // 1. Define your form.
   const [isLoading, setisLoading] = useState(false);
-  
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
     // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+    // This will be type-safe and validated.
+    setisLoading(true);
+
+    try {
+      const userData = { name, email, phone, }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
