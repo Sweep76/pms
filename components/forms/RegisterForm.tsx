62371b3c1161196cb5aeb7e13d3473759659b3e6
Sweep <joshuapatrickchiu76@gmail.com>
2024-09-26 14:55:12 +0800

    feat: added index.ts

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -103,8 +103,8 @@ export const RegisterForm = ({ user }: {user: User}) => { //stack overflow
             label="Gender"
             renderSkeleton={(field) => (
               <FormControl>
-                <RadioGroup className="flex h-11 gap-6 xl:justify-between">
-
+                <RadioGroup className="flex h-11 gap-6 xl:justify-between" onValueChange={field.onChange} defaultValue={field.value}>
+ 
                 </RadioGroup>
               </FormControl>
             )}
