165d2926e677ddefb17be442d4676c6ae8864f42
Sweep <joshuapatrickchiu76@gmail.com>
2024-09-27 14:32:26 +0800

    style: added emergency contact form

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -120,12 +120,46 @@ export const RegisterForm = ({ user }: {user: User}) => { //stack overflow
         />
       </div>
 
+      {/* <section className = "space-y-6">
+        <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Medical Information</h2>
+        </div>
+      </section> */}
+
       <div className="flex flex-col gap-6 xl:flex-row">
-        
+        <CustomFormField
+              fieldType={FormFieldType.INPUT} 
+              control={form.control}
+              name="address"
+              label="Address"
+              placeholder="132 Margosa Street, Cebu City"
+          />
+
+          <CustomFormField
+              fieldType={FormFieldType.INPUT} 
+              control={form.control}
+              name="occupation"
+              label="Occupation"
+              placeholder="Software Engineer"
+          />
       </div>
       
       <div className="flex flex-col gap-6 xl:flex-row">
-        
+          <CustomFormField
+                fieldType={FormFieldType.INPUT} 
+                control={form.control}
+                name="emergencyContactName"
+                label="Emergency contact name"
+                placeholder="Guardian's Name"
+            />
+
+            <CustomFormField
+                fieldType={FormFieldType.PHONE_INPUT} 
+                control={form.control}
+                name="emergencyContactNumber"
+                label="Emergency contact number"
+                placeholder="(555) 123-4567"
+            />
       </div>
 
       <div className="flex flex-col gap-6 xl:flex-row">
