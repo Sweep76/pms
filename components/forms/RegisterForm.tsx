c868e41abca044449d084e44fff5eb86bc128b9d
Sweep <joshuapatrickchiu76@gmail.com>
2024-09-27 14:45:04 +0800

    chore: reformat code layout

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -120,47 +120,55 @@ export const RegisterForm = ({ user }: {user: User}) => { //stack overflow
         />
       </div>
 
-      {/* <section className = "space-y-6">
-        <div className="mb-9 space-y-1">
-            <h2 className="sub-header">Medical Information</h2>
-        </div>
-      </section> */}
-
       <div className="flex flex-col gap-6 xl:flex-row">
         <CustomFormField
-              fieldType={FormFieldType.INPUT} 
-              control={form.control}
-              name="address"
-              label="Address"
-              placeholder="132 Margosa Street, Cebu City"
+            fieldType={FormFieldType.INPUT} 
+            control={form.control}
+            name="address"
+            label="Address"
+            placeholder="132 Margosa Street, Cebu City"
           />
 
-          <CustomFormField
-              fieldType={FormFieldType.INPUT} 
-              control={form.control}
-              name="occupation"
-              label="Occupation"
-              placeholder="Software Engineer"
+        <CustomFormField
+            fieldType={FormFieldType.INPUT} 
+            control={form.control}
+            name="occupation"
+            label="Occupation"
+            placeholder="Software Engineer"
           />
       </div>
       
       <div className="flex flex-col gap-6 xl:flex-row">
-          <CustomFormField
-                fieldType={FormFieldType.INPUT} 
-                control={form.control}
-                name="emergencyContactName"
-                label="Emergency contact name"
-                placeholder="Guardian's Name"
-            />
+        <CustomFormField
+            fieldType={FormFieldType.INPUT} 
+            control={form.control}
+            name="emergencyContactName"
+            label="Emergency contact name"
+            placeholder="Guardian's Name"
+        />
 
-            <CustomFormField
-                fieldType={FormFieldType.PHONE_INPUT} 
-                control={form.control}
-                name="emergencyContactNumber"
-                label="Emergency contact number"
-                placeholder="(555) 123-4567"
-            />
+        <CustomFormField
+            fieldType={FormFieldType.PHONE_INPUT} 
+            control={form.control}
+            name="emergencyContactNumber"
+            label="Emergency contact number"
+            placeholder="(555) 123-4567"
+        />
       </div>
+
+      <section className = "space-y-6">
+        <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Medical Information</h2>
+        </div>
+      </section>
+
+        <CustomFormField
+            fieldType={FormFieldType.SELECT} 
+            control={form.control}
+            name="primaryPhysician"
+            label="Primary Physician"
+            placeholder="Select a physician"
+        />
 
       <div className="flex flex-col gap-6 xl:flex-row">
         
