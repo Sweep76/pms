87baa76190a55b09e0bb1d8c9b25c891e6cad075
Sweep <joshuapatrickchiu76@gmail.com>
2024-10-31 16:05:15 +0800

    feat: added fileuploader component

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -17,6 +17,7 @@ import { Doctors, GenderOptions, IdentificationTypes } from "@/constants";
 import { Label } from "../ui/label";
 import { SelectItem } from "../ui/select";
 import Image from "next/image";
+import FileUploader from "../FileUploader";
 
  
 export const RegisterForm = ({ user }: {user: User}) => { //stack overflow
@@ -260,6 +261,26 @@ export const RegisterForm = ({ user }: {user: User}) => { //stack overflow
             </SelectItem>
           ))}
         </CustomFormField>
+
+        <CustomFormField
+            fieldType={FormFieldType.INPUT} 
+            control={form.control}
+            name="identificationNumber"
+            label="Identification number"
+            placeholder="123456789"
+          />
+
+        <CustomFormField
+            fieldType={FormFieldType.SKELETON} 
+            control={form.control}
+            name="identificationDocument"
+            label="Scanned copy of identification document"
+            renderSkeleton={(field) => (
+             <FormControl>
+              <FileUploader />
+             </FormControl>
+            )}
+        />          
       
       <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
     </form>
