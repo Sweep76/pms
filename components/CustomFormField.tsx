6c89a1ce40763fbee8b73182f27a73ad067d94ba
Sweep <joshuapatrickchiu76@gmail.com>
2024-10-31 16:24:19 +0800

    feat: added checkbox formfield

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -24,6 +24,7 @@ import { render } from "react-dom"
 import { Select, SelectContent, SelectTrigger } from "./ui/select"
 import { SelectValue } from "@radix-ui/react-select"
 import { Textarea } from "./ui/textarea"
+import { Checkbox } from "./ui/checkbox"
 
 interface CustomProps {
   control: Control<any>,
@@ -132,6 +133,19 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
       )
     case FormFieldType.SKELETON:
       return renderSkeleton ? renderSkeleton(field) : null
+    case FormFieldType.CHECKBOX:
+      return (<FormControl>
+        <div className="flex items-center gap-4">
+          <Checkbox 
+            id={props.name}
+            checked={field.value}
+            onCheckedChange={field.onChange}
+          />
+          <label htmlFor={props.name} className="checkbox-label">
+            {props.label}
+          </label>
+        </div>
+      </FormControl>)
     default:
       break;
   }
