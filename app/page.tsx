8f1572e97ee4d49a453dbb337b5abcc5f3733a81
Sweep76 <joshuapatrickchiu76@gmail.com>
2025-07-22 06:55:15 +0800

    refactor(page): reorganize imports and improve component structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
-import PatientForm from '@/components/forms/PatientForm';
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+import { PatientForm } from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verification | Passkey Modal */}
+      {isAdmin && <PasskeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <Image 
+          <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
@@ -19,9 +23,9 @@ export default function Home() {
 
           <PatientForm />
 
-          <div className= "text-14-regular mt-20 flex justify-between">
-            <p className='justify-items-end text-dark-600 xl:text-left'>
-              © 2024 CarePulse
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePluse
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
@@ -30,7 +34,7 @@ export default function Home() {
         </div>
       </section>
 
-      <Image 
+      <Image
         src="/assets/images/onboarding-img.png"
         height={1000}
         width={1000}
@@ -38,5 +42,7 @@ export default function Home() {
         className="side-img max-w-[50%]"
       />
     </div>
-  )
-}
+  );
+};
+
+export default Home;
