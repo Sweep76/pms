a3392fed17be0b28b2cb2ccafbb7f326d18c870a
Sweep76 <joshuapatrickchiu76@gmail.com>
2025-07-28 21:31:26 +0800

    refactor(Appointment): restructure component and improve code clarity

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,44 +1,42 @@
-import AppointmentForm from '@/components/forms/AppointmentForm';
-import PatientForm from '@/components/forms/PatientForm';
-import { getPatient } from '@/lib/actions/patient.actions';
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
 
-export default async function NewAppointment({params: {userId}}: SearchParamProps) {
+import { AppointmentForm } from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+
+const Appointment = async ({ params: { userId } }: SearchParamProps) => {
   const patient = await getPatient(userId);
 
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[860px] flex-1 justify-between">
-          <Image 
+          <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
-            alt="patient"
+            alt="logo"
             className="mb-12 h-10 w-fit"
           />
 
-          <AppointmentForm 
-            type="create"
+          <AppointmentForm
+            patientId={patient?.$id}
             userId={userId}
-            patientId={patient?.$id} // stack overflow guide
+            type="create"
           />
 
-            <p className='copyright mt-10 py-12'>
-              © 2024 CarePulse
-            </p>
-
+          <p className="copyright mt-10 py-12">© 2024 CarePluse</p>
         </div>
       </section>
 
-      <Image 
+      <Image
         src="/assets/images/appointment-img.png"
-        height={1000}
-        width={1000}
+        height={1500}
+        width={1500}
         alt="appointment"
         className="side-img max-w-[390px] bg-bottom"
       />
     </div>
-  )
-}
+  );
+};
+
+export default Appointment;
